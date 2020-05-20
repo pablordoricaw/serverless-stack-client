5b7189f80ceff1d1b11598b2be2a871807014d9c
Pablo Ordorica Wiener <pablo.ordoricaw@gmail.com>
2020-05-19 22:17:21 -0400

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-71",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://xqc5mn45kd.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_szCwjrm7m",
+    APP_CLIENT_ID: "63g510e5pfieuq90l5m5c2ttk4",
+    IDENTITY_POOL_ID: "us-east-1:630bee52-9673-402c-befe-61079787725f",
+  },
+};
