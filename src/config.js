0e64996b858a733f2ee652cc2a43df321e6d896c
Pablo Ordorica Wiener <pablo.ordoricaw@gmail.com>
2020-05-31 18:30:03 -0400

    Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,18 +1,44 @@
-export default {
-  STRIPE_KEY: "YOUR_STRIPE_PUBLIC_KEY",
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
+  STRIPE_KEY: "pk_test_ID9G5oNPtC3LhO2l3gC5OkyZ00fI7zXTMt",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-uploads-71",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-52be63cmb7ys",
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://xqc5mn45kd.execute-api.us-east-1.amazonaws.com/prod",
+    URL: "https://api.pablordoricaw.com/dev",
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_szCwjrm7m",
-    APP_CLIENT_ID: "63g510e5pfieuq90l5m5c2ttk4",
-    IDENTITY_POOL_ID: "us-east-1:630bee52-9673-402c-befe-61079787725f",
+    USER_POOL_ID: "us-east-1_ngRFiaiZC",
+    APP_CLIENT_ID: "3rh0bag1odq6vb462vagru8au4",
+    IDENTITY_POOL_ID: "us-east-1:5fc6b4f9-3435-4093-aeb9-261448387ef8",
   },
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_ID9G5oNPtC3LhO2l3gC5OkyZ00fI7zXTMt",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-tmp7n2wfbkz7",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://api.pablordoricaw.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_ngRFiaiZC",
+    APP_CLIENT_ID: "7t3fjmmg6em50qap21a21m58ru",
+    IDENTITY_POOL_ID: "us-east-1:5fc6b4f9-3435-4093-aeb9-261448387ef8",
+  },
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config,
 };
